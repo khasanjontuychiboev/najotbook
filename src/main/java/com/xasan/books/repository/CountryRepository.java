8967d1f269ad1a7745ccb8adfe322cd856ffec5c
khasanjontuychiboev <khasanjontuychiboev@gmail.com>
2023-09-20 15:03:05 +0500

    resolved , starter initialized

--- src/main/java/com/xasan/books/repository/CountryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.xasan.books.repository;
-
-import com.xasan.books.entity.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CountryRepository extends JpaRepository<Country, Integer> {
-}
